6747636f114b7de1f33d824004dcd7a328084c4b
arty gvozdenkov <gvozdenkov@gmail.com>
2024-08-14 16:50:06 +0300

    feat(backend): add mongo setup

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { GatewayModule } from './gateway/gateway.module';
 import { AppConfigModule } from './config/config.module';
+import { DatabaseModule } from './db/database.module';
 
 @Module({
-  imports: [AppConfigModule, GatewayModule],
+  imports: [AppConfigModule, DatabaseModule, GatewayModule],
   controllers: [],
   providers: [],
 })
---
+++ backend/src/db/database.module.ts
@@ -0,0 +1,17 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (configService: ConfigService) => ({
+        uri: configService.get('MONGODB_URI'),
+        dbName: configService.get('DATABASE_NAME'),
+      }),
+    }),
+  ],
+})
+export class DatabaseModule {}
