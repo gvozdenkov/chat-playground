d64ef237b6e9406f703f8ea5900e00c3de4522af
arty gvozdenkov <gvozdenkov@gmail.com>
2024-08-16 09:52:57 +0300

    chore(backend): add global validation pipe

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -5,6 +5,13 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
 
   const globalPrefix = 'api';
 
--- backend/src/users/schemas/user.schema.ts
+++ backend/src/users/schemas/user.schema.ts
@@ -2,6 +2,7 @@ import { Prop, SchemaFactory, Schema } from '@nestjs/mongoose';
 import { Document, SchemaTypes } from 'mongoose';
 
 import { UserRole } from '#common/types';
+import { IsEmail, IsOptional, IsString, IsUrl } from 'class-validator';
 
 @Schema({
   timestamps: true,
@@ -13,21 +14,27 @@ import { UserRole } from '#common/types';
   versionKey: false,
 })
 export class User extends Document {
+  @IsEmail()
   @Prop({ unique: true, required: true, lowercase: true })
   email: string;
 
+  @IsString()
   @Prop({ select: false, required: true })
   password: string;
 
+  @IsOptional()
+  @IsUrl()
   @Prop({
     default: () =>
       `https://i.pravatar.cc/200/?img=${Math.floor(Math.random() * 70) + 1}`,
   })
   avatar: string;
 
+  @IsString()
   @Prop({ required: true, minLength: 2 })
   name: string;
 
+  @IsString()
   @Prop({
     type: SchemaTypes.String,
     required: true,
